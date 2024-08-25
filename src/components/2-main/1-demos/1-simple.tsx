9791e19a3368faea58c927f9ee36124fc3a0faf4
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-24 18:42:28 -0700

    WIP. first demo play. 1

--- src/components/2-main/1-demos/1-simple.tsx
+++ src/components/2-main/1-demos/1-simple.tsx
@@ -3,13 +3,25 @@ import { AnimatePresence, motion } from "framer-motion";
 
 export function Simple() {
     const [show, setShow] = useState(false);
+    const [idDone, setIdDone] = useState(false);
     return (
-        <div className="my-4">
-            <button className="px-2 py-1 min-w-16 border-sky-600 border rounded shadow active:scale-95" type="button" onClick={() => setShow(v => !v)}>
+        <div className="my-4 text-sm">
+            <button
+                className="px-2 py-1 min-w-16 border-sky-600 border rounded shadow active:scale-95"
+                type="button"
+                onClick={() => {
+                    !show && setIdDone(false);
+                    setShow(v => !v);
+                }}
+            >
                 {show ? "Hide" : "Show"}
             </button>
 
-            <AnimatePresence>
+            <div className="border-sky-600 border-b">
+                {idDone ? 'exit animation done' : 'element visible'}
+            </div>
+
+            <AnimatePresence onExitComplete={() => setIdDone(true)}>
                 {show && (
                     <motion.div
                         initial={{
