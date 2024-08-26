7c3b67ed1ca2c1f22f1b8e9a759b1cfa6a2231d3
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-25 18:40:44 -0700

    WIP. add select; update theme primary color to blue

--- src/components/1-header/index.tsx
+++ src/components/1-header/index.tsx
@@ -1,7 +1,21 @@
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "../ui/select";
+
 export function Header() {
     return (
-        <header className="p-4 bg-sky-600">
+        <header className="p-4 bg-sky-600 flex items-center justify-between">
             <h1 className="">Header</h1>
+
+            <Select>
+                <SelectTrigger className="w-[180px]">
+                    <SelectValue placeholder="Select a fruit" />
+                </SelectTrigger>
+                
+                <SelectContent>
+                    <SelectGroup>
+                        <SelectItem value="apple">Apple</SelectItem>
+                    </SelectGroup>
+                </SelectContent>
+            </Select>
         </header>
     );
 }
