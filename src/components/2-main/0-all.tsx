b457c6fa01644f79e1bc1c84fb178e126d257190
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-28 17:51:13 -0700

    WIP. demos. fix demo switch animation

--- src/components/2-main/0-all.tsx
+++ src/components/2-main/0-all.tsx
@@ -4,15 +4,36 @@ import { AnimatePresence, motion } from "framer-motion";
 import { Demo1Simple } from "./1-demos/1-simple";
 import { Demo2Accordion } from "./1-demos/2-accordion";
 
+function MotionWrapper({ children }: { children: React.ReactNode; }) {
+    return (
+        <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 5 }}
+        >
+            {children}
+        </motion.div>
+    );
+}
+
 function AllDemos() {
     const demo = useSnapshot(appSettings).demo;
 
     if (demo === "simple") {
-        return <Demo1Simple key={"simple"} />;
+        return (
+            <MotionWrapper key={"simple"}>
+                <Demo1Simple />
+            </MotionWrapper>
+        );
     }
 
     if (demo === "accordion") {
-        return <Demo2Accordion key={"accordion"} />;
+        return (
+            <MotionWrapper key={"accordion"}>
+                <Demo2Accordion />
+            </MotionWrapper>
+        );
     }
 }
 
