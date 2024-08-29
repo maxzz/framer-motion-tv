5fc22aaaf26fa40bba71007dc74025b2d8fa9edd
Max Zakharzhevskiy <maxzz@msn.com>
2024-08-28 17:47:51 -0700

    WIP. demos. add component keys

--- src/components/2-main/0-all.tsx
+++ src/components/2-main/0-all.tsx
@@ -1,5 +1,6 @@
 import { useSnapshot } from "valtio";
 import { appSettings } from "@/store";
+import { AnimatePresence, motion } from "framer-motion";
 import { Demo1Simple } from "./1-demos/1-simple";
 import { Demo2Accordion } from "./1-demos/2-accordion";
 
@@ -7,18 +8,27 @@ function AllDemos() {
     const demo = useSnapshot(appSettings).demo;
 
     if (demo === "simple") {
-        return <Demo1Simple />;
+        return <Demo1Simple key={"simple"} />;
     }
 
     if (demo === "accordion") {
-        return <Demo2Accordion />;
+        return <Demo2Accordion key={"accordion"} />;
     }
 }
 
 export function Main() {
     return (
         <main className="p-4">
-            <AllDemos />
+            <AnimatePresence initial={false}>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ duration: 5 }}
+                >
+                    <AllDemos />
+                </motion.div>
+            </AnimatePresence>
         </main>
     );
 }
--- src/components/2-main/1-demos/2-accordion.tsx
+++ src/components/2-main/1-demos/2-accordion.tsx
@@ -1,7 +1,11 @@
 export function Demo2Accordion() {
     return (
         <div className="my-4 text-sm">
-            <div className="">Accordion</div>
+            
+            <div className="size-64 bg-teal-900 rounded-md flex items-center justify-center">
+                <div className="text-teal-300">Accordion</div>
+            </div>
+
         </div>
     );
 }
